cb20a11d7b16ebced217adc18dc55e3b4e5fd79c
leeorrH <39300099+leeorrH@users.noreply.github.com>
2019-03-24 11:22:01 +0200

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let port = process.env.PORT || 3000;
-let app = express();
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/ex1.html');
-});app.listen(port, () => {
-	console.log('Example app listening on port %d!', port);
-});
